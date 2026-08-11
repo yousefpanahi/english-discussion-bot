a2e4d2d86775dba3610fbb13aafd39caf20d37d7
Yousef Panahi <89208681+yousefpanahi@users.noreply.github.com>
2026-08-12 00:41:16 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,19 +19,22 @@ def send_message(text):
     print(response.json())
 
 
-# Get current UTC time
+# Current UTC date and time
 now = datetime.now(timezone.utc)
 
 day = now.strftime("%A")
 hour = now.hour
 minute = now.minute
 
-# Session days
+# Our discussion days
 session_days = ["Saturday", "Monday", "Wednesday"]
 
 
-# 1. Class reminder — 13:30 UTC
-if day in session_days and hour == 13 and minute < 30:
+# ---------------------------------------------------
+# 1. CLASS REMINDER — 13:30 UTC
+# ---------------------------------------------------
+
+if day in session_days and hour == 13 and 25 <= minute <= 45:
 
     message = """Hello Everyone. Kindly Reminder
 
@@ -40,8 +43,11 @@ We will have the English Free Discussion meeting today at 4:30 PM - 5:30 PM (UTC
     send_message(message)
 
 
-# 2. Join class — 16:30 UTC
-elif day in session_days and hour == 16 and minute < 30:
+# ---------------------------------------------------
+# 2. JOIN CLASS — 16:30 UTC
+# ---------------------------------------------------
+
+elif day in session_days and hour == 16 and 25 <= minute <= 45:
 
     message = """Everybody please join Class.
 
